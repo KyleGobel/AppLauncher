9e49621e8dfe10db4fc7e6546cd0d748cca203fd
Kyle Gobel <kgobel@gmail.com>
2013-08-31 15:34:12 -0400

    initial push to github

--- GPSLauncher.Main/GPSLauncher/ViewModels/MainViewModel.cs
+++ GPSLauncher.Main/GPSLauncher/ViewModels/MainViewModel.cs
@@ -8,8 +8,8 @@ namespace GPSLauncher.ViewModels
     {
         public MainViewModel()
         {
-            Item1x1 = new IndividualIconViewModel {Caption = "Foo!"};
-            Item1x2 = new IndividualIconViewModel {Caption = "Hello App!", ImageSource = "Icons/menu.png"};
+            Item1x1 = new IndividualIconViewModel {Caption = "Foo Bar!"};
+            Item1x2 = new IndividualIconViewModel { Caption = "VS 2012", ImageSource = "http://fc06.deviantart.net/fs70/i/2012/344/9/1/flurry_ios_visual_studio_2012_replacement_icon_by_flakshack-d5nnelp.png" };
         }
         private IndividualIconViewModel _item1x1;
         public IndividualIconViewModel Item1x1
