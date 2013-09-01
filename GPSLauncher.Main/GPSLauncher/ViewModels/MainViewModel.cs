a63cd979b172b8dd75639a1f680778d48aae1e31
Kyle Gobel <kgobel@gmail.com>
2013-09-01 02:08:02 -0400

    added shortcuts 1-4 hard coded

--- GPSLauncher.Main/GPSLauncher/ViewModels/MainViewModel.cs
+++ GPSLauncher.Main/GPSLauncher/ViewModels/MainViewModel.cs
@@ -8,12 +8,29 @@ namespace GPSLauncher.ViewModels
     {
         public MainViewModel()
         {
-            Item1x1 = new IndividualIconViewModel {Caption = "Foo Bar!"};
+            Item1x1 = new IndividualIconViewModel
+                {
+                    Caption = "GitHub",
+                    Command = @"C:\Users\Kyle\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\GitHub, Inc\GitHub.appref-ms",
+                    ImageSource = "http://files.softicons.com/download/social-media-icons/glowing-social-network-icons-by-aaron-nichols/png/600/GitHub_Icon.png"
+                };
             Item1x2 = new IndividualIconViewModel
                 {
                     Caption = "VS 2012", 
                     ImageSource = "http://fc06.deviantart.net/fs70/i/2012/344/9/1/flurry_ios_visual_studio_2012_replacement_icon_by_flakshack-d5nnelp.png",
-                    Command = "http://visualstudio.com"
+                    Command = @"C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe"
+                };
+            Item1x3 = new IndividualIconViewModel
+                {
+                    Caption = "Sublime",
+                    ImageSource = "http://c758482.r82.cf2.rackcdn.com/sublime_text_icon_2181.png",
+                    Command = @"D:\Applications\Sublime Text 2\sublime_text.exe"
+                };
+            Item1x4 = new IndividualIconViewModel
+                {
+                    Caption = "GMusic",
+                    ImageSource = "http://wcdn4.dataknet.com/static/resources/icons/set31/7e819aff.png",
+                    Command = "http://music.google.com/"
                 };
         }
         private IndividualIconViewModel _item1x1;
@@ -44,6 +61,32 @@ namespace GPSLauncher.ViewModels
             }
         }
 
+        private IndividualIconViewModel _item1x3;
+        public IndividualIconViewModel Item1x3
+        {
+            get
+            {
+                return _item1x3;
+            }
+            set
+            {
+                _item1x3 = value;
+                OnPropertyChanged();
+            }
+        }
+        private IndividualIconViewModel _item1x4;
+        public IndividualIconViewModel Item1x4
+        {
+            get
+            {
+                return _item1x4;
+            }
+            set
+            {
+                _item1x4 = value;
+                OnPropertyChanged();
+            }
+        }
         public event PropertyChangedEventHandler PropertyChanged;
 
         [NotifyPropertyChangedInvocator]
