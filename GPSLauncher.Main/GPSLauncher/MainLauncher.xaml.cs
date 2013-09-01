f9c20dabf60510f72d42c6019df7d88ff26a5142
Kyle Gobel <kgobel@gmail.com>
2013-09-01 01:53:41 -0400

    functional search

--- GPSLauncher.Main/GPSLauncher/MainLauncher.xaml.cs
+++ GPSLauncher.Main/GPSLauncher/MainLauncher.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Forms;
+using System.Windows.Input;
+using KeyEventArgs = System.Windows.Input.KeyEventArgs;
 
 namespace GPSLauncher
 {
@@ -32,6 +35,26 @@ namespace GPSLauncher
         public static int GetTaskbarHeight()
         {
             return Screen.PrimaryScreen.Bounds.Height - Screen.PrimaryScreen.WorkingArea.Height;
+        }
+
+        private void SearchBox_OnGotFocus(object sender, RoutedEventArgs e)
+        {
+            if (SearchBox.Text == "Search")
+                SearchBox.Text = "";
+        }
+
+        private void SearchBox_OnLostFocus(object sender, RoutedEventArgs e)
+        {
+            if (SearchBox.Text == "")
+                SearchBox.Text = "Search";
+        }
+
+        private void SearchBox_OnKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Process.Start("http://www.google.com/search?q=" + SearchBox.Text);
+            }
         }
     }
 }
